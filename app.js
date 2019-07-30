b223cd7cc358d8f6e9855d0c695607f4f2f5f0d2
leean <leeeeean91@gmail.com>
2019-07-31 04:05:48 +0900

    Create signin

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+var session = require('express-session');
+var flash    = require('connect-flash');
+var methodOverride = require('method-override');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.locals.userid = null
+app.locals.username = null
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,16 +25,32 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
+
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   indentedSyntax: true, // true = .sass and false = .scss
   sourceMap: true
 }));
+
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'long-long-long-secret-string-1313513tefgwdsvbjkvasd'
+}));
+
+app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.locals.flashMessages = req.flash();
+  next();
+});
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
-var express = require('express');
+var express = require('express')
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/signin', (req, res, next) => {
+    res.render('signin');
 });
+
+router.post('/signin/1', (req, res, next) =>  {
+    req.app.locals.userid = "1";
+    req.app.locals.username = "등록자";
+    req.flash('success', 'Welcome, 등록자!');
+    res.redirect('back')
+});
+
+router.post('/signin/2', (req, res, next) =>  {
+    req.app.locals.userid = "2";
+    req.app.locals.username = "평가자";
+    req.flash('success', 'Welcome, 평가자!');
+    res.redirect('back')
+});
+
+router.post('/signin/3', (req, res, next) =>  {
+  req.app.locals.userid = "3";
+  req.app.locals.username = "플랫폼";
+  req.flash('success', 'Welcome, 플랫폼!');
+  res.redirect('back')
+});
+
+router.post('/signin/4', (req, res, next) =>  {
+  req.app.locals.userid = "4";
+  req.app.locals.username = "투자자";
+  req.flash('success', 'Welcome, 투자자!');
+  res.redirect('back')
+});
+
+router.get('/signout', (req, res) => {
+    req.app.locals.userid = null;
+    req.app.locals.username = null;
+    req.flash('success', 'Successfully signed out');
+    res.redirect('/')
+})
 
 module.exports = router;
